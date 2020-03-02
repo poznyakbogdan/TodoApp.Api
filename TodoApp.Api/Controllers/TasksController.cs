c134264345c55a39a544a4352f0d298734781241
poznyakbogdan <poznyak.pba@gmail.com>
2020-03-02 15:59:57 +0300

    initialaize basic project structure

---
+++ TodoApp.Api/Controllers/TasksController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController : ControllerBase
+    {
+    }
+}
